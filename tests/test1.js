413a161a74c479523391d9d41347a6e9be3954ca
Kevin Deyne <kevindeyne@gmail.com>
2018-03-20 22:59:51 -0700

    Using a NodeJS server to run testcafe tests

--- tests/test1.js
+++ tests/test1.js
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe';
 
 fixture `Getting Started`
-	.page `index.html`;
+	.page `http://localhost:9090/index.html`;
 
 test('My first test', async t => {
 // Test code
